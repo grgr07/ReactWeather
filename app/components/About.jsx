a1c037d91fc88fb5435c958d20b81d6a8f7a8eba
Gregory Ravizvah <gregoryr@top4.com>
2019-01-28 15:47:21 +0200

    styled about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -11,8 +11,8 @@ var React = require('react');
 var About = (props) => {
     return (
             <div>
-              <h3>About</h3>
-              <p>Welcome to about page</p>
+              <h1 className="text-center">About</h1>
+              <p>This is a weather application based React. I built is as a part of study. </p>
               </div>
              );
 }
